aec42121d367b7a85a371070a6fb4bd339d7be8d
Iston-Feanor <ipgtdan@gmail.com>
2024-01-10 10:54:24 -0300

    Implementando as variaveis e metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "303 505 7824", modelo: "Modelo XY", imei: "777777777", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone (numero: "987654321", modelo: "Modelo 27", imei: "333333333", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
